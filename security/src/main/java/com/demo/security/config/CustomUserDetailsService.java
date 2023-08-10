282f07e8b4145680a428fb8f617787d9dfa051bb
SaraMohamedSalah <sasos2011@live.com>
2023-08-10 09:33:53 +0300

    Add files via upload

---
+++ security/src/main/java/com/demo/security/config/CustomUserDetailsService.java
@@ -0,0 +1,16 @@
+package com.demo.security.config;
+
+import com.demo.security.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = new User("Admin", "password");
+        return user;
+    }
+}
