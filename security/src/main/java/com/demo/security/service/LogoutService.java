26a6830ef77202d3b78c37614059ad1c7609696d
SaraMohamedSalah <sasos2011@live.com>
2023-08-15 10:02:14 +0300

    Add files via upload

---
+++ security/src/main/java/com/demo/security/service/LogoutService.java
@@ -0,0 +1,35 @@
+package com.demo.security.service;
+
+import com.demo.security.config.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserCache;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+    private final JwtUtils jwtUtils;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        if (jwt != null) {
+
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
+            System.out.println("The user " + username + " has logged out.");
+            SecurityContextHolder.clearContext();
+        }
+    }
+}
